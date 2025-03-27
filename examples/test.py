f7b5dc073a5c23b96d0becb1016009a7cb58f66b
Yiwen Wang <yiwenwang9702@gmail.com>
2025-03-26 21:56:34 -0700

    dummy example

--- examples/test.py
+++ examples/test.py
@@ -1,11 +1,11 @@
 import aloe
 
-engine = aloe.aloe(input_file = "test.csv")
-engine.add_step(aloe.StereoIsoConfig()) # Generate stereoisomers
-engine.add_step(aloe.ConformerConfig()) # Ember conformers
-engine.add_step(aloe.OptConfig()) # Optimize conformers
-engine.add_step(aloe.RankConfig(k=3)) # Rank optimized conformers, pick the best 3
-engine.add_step(aloe.ThermoConfig()) # Thermochemistry calculations via ASE
-output_file = engine.run() # Asynchronous execution
+engine = aloe.aloe(input_file="test.csv")
+engine.add_step(aloe.StereoIsoConfig())  # Generate stereoisomers
+engine.add_step(aloe.ConformerConfig())  # Ember conformers
+engine.add_step(aloe.OptConfig())  # Optimize conformers
+engine.add_step(aloe.RankConfig(k=3))  # Rank optimized conformers, pick the best 3
+engine.add_step(aloe.ThermoConfig())  # Thermochemistry calculations via ASE
+output_file = engine.run()  # Asynchronous execution
 
 print(output_file)
